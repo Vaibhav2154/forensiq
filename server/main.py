4383f611cc11a8ca43cc451b60f3612d8f4223ff
Vaibhav2154 <vaibhavvaibhu2005@gmail.com>
2025-08-30 04:15:04 +0530

    update

--- server/main.py
+++ server/main.py
@@ -146,7 +146,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8000,
-        reload=True,
+        port=8000,,
         log_level=Config.LOG_LEVEL.lower()
     )
